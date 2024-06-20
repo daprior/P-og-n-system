4b7594137023f0fadec72bf5385e242cf42a6e14
Daniel Prior <daniel_wow1@live.dk>
2024-06-20 08:06:08 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,15 +32,10 @@ export default function Index() {
         <img src="/logo.png" className="h-16 w-48"></img>
 
         <div className="pt-10 pr-20 pl-20 pb-20 shadow-xl mt-10 rounded-lg bg-white">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Opret medarbejder V1
-          </h2>
-          <Onboard />
-        </div>
-
-        {/* Buttons in the right corner */}
-        <div className="absolute top-0 right-0 m-4">
-          <Group>
+          <div className="flex justify-between mb-5">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Opret medarbejder
+            </h2>
             <Button
               size="md"
               className="bg-black"
@@ -48,6 +43,14 @@ export default function Index() {
             >
               Åben tjekliste
             </Button>
+          </div>
+
+          <Onboard />
+        </div>
+
+        {/* Buttons in the right corner */}
+        <div className="absolute top-0 right-0 m-4">
+          <Group>
             <Link href="/login">
               <Button size="md" className="bg-black">
                 Gå til Login
